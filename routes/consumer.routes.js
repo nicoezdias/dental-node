304fbda9d9b7597a1071f0d2bd2766e2beb81078
nicoezdias <nicolasezdias@gmail.com>
2023-02-06 15:37:15 -0300

    structure and models

--- routes/consumer.routes.js
+++ routes/consumer.routes.js
@@ -8,18 +8,4 @@ const router = express.Router();
 
 router.get('/', consumerController.getIndex);
 
-// router.get('/products', consumerController.getProducts);
-
-// router.get('/products/:productId', consumerController.getProduct);
-
-// router.get('/cart', consumerController.getCart);
-
-// router.post('/cart', consumerController.postCart);
-
-// router.post('/cart-delete-item', consumerController.postCartDeleteProduct);
-
-// router.post('/create-order', consumerController.postOrder);
-
-// router.get('/orders', consumerController.getOrders);
-
 module.exports = router;
